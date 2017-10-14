6195d9a6b135ffd76704340406bcc592f7c0c739
jbk1109 <jbk1109@gmail.com>
2017-10-13 21:25:31 -0700

    Add files via upload

---
+++ hikingData.js
@@ -0,0 +1,10 @@
+var data = {"type":"FeatureCollection","features":[
+{"type":"Feature","id":"01","properties":{"Name":"Camp Muir","ElevationGain":4640, "HighestElevation" : 10080, "Length":8,"Type":"Round Trip"},"geometry":{"type":"Point","coordinates":[-121.7350,46.7860]}},
+{"type":"Feature","id":"02","properties":{"Name":"Cascade Pass & Sahale Arm","ElevationGain":4000, "HighestElevation" : 7570, "Length":12,"Type":"Round Trip"},"geometry":{"type":"Point","coordinates":[-121.0750,48.4755]}},
+{"type":"Feature","id":"03","properties":{"Name":"High Rock","ElevationGain":1365, "HighestElevation" : 5685, "Length":3.2,"Type":"Round Trip"},"geometry":{"type":"Point","coordinates":[-121.8913,46.6664]}},
+{"type":"Feature","id":"04","properties":{"Name":"Mt. Ellinor","ElevationGain":3300, "HighestElevation" : 5944, "Length":6.2,"Type":"Round Trip"},"geometry":{"type":"Point","coordinates":[-123.2321,47.5066]}},
+{"type":"Feature","id":"05","properties":{"Name":"Chain Lakes Loop Trail","ElevationGain":1700, "HighestElevation" : 5400, "Length":8,"Type":"Round Trip"},"geometry":{"type":"Point","coordinates":[-121.6925,48.8469]}},
+{"type":"Feature","id":"06","properties":{"Name":"Heather-Maple Pass Loop","ElevationGain":2000, "HighestElevation" : 6650, "Length":7.2,"Type":"Round Trip"},"geometry":{"type":"Point","coordinates":[-120.7354,48.5162]}}
+]};
+
+
